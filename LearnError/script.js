0c703aa85f3dcf4db36b2659415ba23734bfd12e
alyachaerul <ralyachaerul@gmail.com>
2021-02-04 21:42:30 +0700

    Learn Error

---
+++ LearnError/script.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "Alya",
+    age: 22,
+    address: "Bandung street"
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+setTimeout(() => {
+    console.log("This is error from set time out")
+}, 100);
